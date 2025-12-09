f39c79914377692c193e6a1019e4a54aa2ec7201
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-12-09 09:40:16 +0530

    added category-list page and router to it

--- UI/CodePulse/src/app/app.routes.ts
+++ UI/CodePulse/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/category/category-list/category-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'admin/categories', component: CategoryList}
+];
---
+++ UI/CodePulse/src/app/features/category/add-category/add-category.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-category',
+  imports: [],
+  templateUrl: './add-category.html',
+  styleUrl: './add-category.css',
+})
+export class AddCategory {
+
+}
