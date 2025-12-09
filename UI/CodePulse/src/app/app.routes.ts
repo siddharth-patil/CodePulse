d8d153d3a1f7d34fbf0d19a2a7527afe94a83a48
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-12-09 10:01:55 +0530

    add-category page with reactive form

--- UI/CodePulse/src/app/app.routes.ts
+++ UI/CodePulse/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CategoryList } from './features/category/category-list/category-list';
+import { AddCategory } from './features/category/add-category/add-category';
 
 export const routes: Routes = [
-    {path:'admin/categories', component: CategoryList}
+    {path:'admin/categories', component: CategoryList},
+    {path:'admin/categories/add', component: AddCategory},
 ];
--- UI/CodePulse/src/app/features/category/add-category/add-category.ts
+++ UI/CodePulse/src/app/features/category/add-category/add-category.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './add-category.html',
   styleUrl: './add-category.css',
 })
 export class AddCategory {
+// 1. Import Reactive forms module
+// 2. FormGroups -> FomrControls
+
+
+addCategoryFormGroup = new FormGroup({
+  name: new FormControl<string>('',{nonNullable: true}),
+  urlHandle: new FormControl<string>('',{nonNullable: true}),
+});
+
+onSubmit(){
+  console.log(this.addCategoryFormGroup.getRawValue());
+  
+}
+
 
 }
